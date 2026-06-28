55e229f3941c810c9a644f6959d5629a455e506d
Martina Drlík <533091+martindrlik@users.noreply.github.com>
2026-06-28 22:11:54 +0200

    improvements

---
+++ src/lib/post/PostLinks.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { posts } from './Data.svelte';
+	import PostLink from './PostLink.svelte';
+</script>
+
+<ul>
+	{#each posts as post (post.headline)}
+		<li>
+			<PostLink {post} />
+		</li>
+	{/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
-	import { posts } from '$lib/post/Data.svelte';
-	import PostLink from '$lib/post/PostLink.svelte';
+	import PostLinks from '$lib/post/PostLinks.svelte';
 </script>
 
-<ul>
-	<!-- todo {@render link('Zig: async', resolve('/zig/async'))} -->
-	<!-- todo {@render link('Terminal: Mini-Tricks', resolve('/terminal/minitricks'))} -->
-	{#each posts as post (post.headline)}
-		<li>
-			<PostLink {post} />
-		</li>
-	{/each}
-</ul>
+<PostLinks />
